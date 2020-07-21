6e378b045ecbe7bf353cee80e9c7e6ee04a84faf
Solidgeek <thesolidgeek@gmail.com>
2020-07-21 03:24:31 +0200

    Enabling DFU Button

--- config/sdk_config.h
+++ config/sdk_config.h
@@ -126,7 +126,7 @@
 // <e> NRF_BL_DFU_ENTER_METHOD_BUTTON - Enter DFU mode on button press.
 //==========================================================
 #ifndef NRF_BL_DFU_ENTER_METHOD_BUTTON
-#define NRF_BL_DFU_ENTER_METHOD_BUTTON 0
+#define NRF_BL_DFU_ENTER_METHOD_BUTTON 1
 #endif
 // <o> NRF_BL_DFU_ENTER_METHOD_BUTTON_PIN  - Button for entering DFU mode.
  
